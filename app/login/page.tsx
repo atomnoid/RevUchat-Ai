66f9fba91a806cbec2aa2e99f19ffcf181547147
atomnoid <aayushs2580@gmail.com>
2026-05-01 13:19:22 +0530

    fixed typo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -140,7 +140,7 @@ export default function LoginPage() {
         </div>
 
         <p className="text-center text-sm text-white/30 mt-6">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link href="/signup" className="text-[#39ff87] hover:underline">Sign up</Link>
         </p>
       </div>
